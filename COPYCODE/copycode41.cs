68b268b4e90474e240ff6c6ad7124792447cc24d
nithish <nithishtulalu121@gmail.com>
2025-09-13 02:54:41 +0530

    41com

---
+++ COPYCODE/copycode41.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace COPYCODE
+{
+    //Removing  Duplicates   from  a  given  string 
+    public class copycode41
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string ::");
+            string str = Console.ReadLine();
+            HashSet<char> set = new HashSet<char>();
+            string result = " ";
+            foreach (char c in str)
+            {
+                if (set.Add(c))
+                {
+                    result = result + c;
+                }
+                
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
