6df12bf1d16c8e310acb25dc8dac7d4714d2e4a5
nithish <nithishtulalu121@gmail.com>
2025-09-17 23:24:28 +0530

    na

---
+++ COPYCODE/copycode42.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace COPYCODE
+{
+    //    Togale case
+    //Lowercase → Uppercase
+
+    //Uppercase → Lowercase
+
+    //Numbers & special characters remain the same
+    public class copycode42
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enater a string ::");
+            string str = Console.ReadLine();
+            string output = " ";
+            foreach (char c in str)
+            {
+                if (char.IsLower(c))
+                {
+                    output = output + char.ToUpper(c);
+                }
+                else if (char.IsUpper(c))
+                {
+                    output = output + char.ToLower(c);
+                }
+                else
+                {
+                    output = output + c;
+                }
+
+            }
+            Console.WriteLine(str);
+            Console.WriteLine(output);
+        }
+    }
+}
